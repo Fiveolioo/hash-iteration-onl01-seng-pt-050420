742f13fda87c1b59f9ca4200894b795ac2206fdb
Richard Cho <96richardohc@gmail.com>
2020-05-14 06:35:21 +0000

    Automatically backed up by Learn

--- lib/birthday.rb
+++ lib/birthday.rb
@@ -14,8 +14,8 @@ def age_appropriate_birthday(birthday_kids)
   birthday_kids.each do |kid, age|
     if age <= 12
       puts "Happy Birthday #{kid}! You are now #{age} years old!"
-    end 
     else
       puts "You are too old for this."
+    end
   end
 end
